65a4a64581288753c057179cb112add253c30fcc
Nikita <monolit9677@mail.ru>
2022-09-18 14:05:27 +0700

    задача2

---
+++ Python/Seminar5/task2.py
@@ -0,0 +1,18 @@
+# НОД двух чисел
+
+from re import A
+
+
+# while a != b:
+#     if a > b:
+#         a -= b
+#     else:
+#         b -= a 
+# print(a)
+
+
+# while b != 0:
+#     a, b = b, a % b
+
+# print(a)
+
