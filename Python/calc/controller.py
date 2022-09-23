7013b6de0f40a053635abdd834de7903f2b95578
Nikita <monolit9677@mail.ru>
2022-09-23 13:31:04 +0700

    controller

--- Python/calc/controller.py
+++ Python/calc/controller.py
@@ -1,11 +1,16 @@
 from statistics import mode
 import  model_exp as model
+# import  model_mult as model
+# import  model_sub as model
+# import  model_dev as model
+# import  model_sum as model
 import view
+
 
 def button_click():
     value_a = view.get_value()
     value_b = view.get_value()
-    num = view.get_oper()
+    num = view.get_opera()
     model.init(value_a, value_b)
     result = model.do_it()
     view.view_data(result, "resul")
