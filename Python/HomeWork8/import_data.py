fac0393e8b171ff7a453bd569302c0d6ecbd0ab5
Nikita <monolit9677@mail.ru>
2022-09-29 23:56:31 +0700

    im

---
+++ Python/HomeWork8/import_data.py
@@ -0,0 +1,10 @@
+
+def import_data(data, sep = None):
+    with open('system.csv', 'a+', encoding='utf-8') as file:
+        if sep == None:
+            for i in data:
+                file.write(f"{i}\n")
+            file.write(f"\n")
+        else:
+            file.write(sep.join(data))
+            file.write(f"\n")
