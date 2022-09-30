e987e11c088d1293d6e3b6cdbd0efa2baa3287b5
Nikita <monolit9677@mail.ru>
2022-09-30 21:52:31 +0700

    main

---
+++ Python/venv/main.py
@@ -0,0 +1,27 @@
+# from asyncore import dispatcher
+# from telegram import Update, Bot
+# from telegram.ext import Update, CommandHandler, MessageHandler
+# from random import randint
+
+# bot_token ="5629995312:AAG0f7SAmzTQIY8qtS_6xD7rMBmf95Nlqwc"
+# bot = Bot(bot_token)
+# updater = Updater(bot_token, use_context=True)
+# dispatcher = updater.dispatcher
+# def start(update, context):
+#     context.bot.send_messeage(update.effective_chat.id, "Привет!")
+
+# start_handler = CommandHandler('start', start)
+# def info(update, context):
+#     context.bot.send_message(update.effective_chat.id, "это бот")
+# def roll(update, context):
+#     context.bot.send_message(update.effective_chat.id, text = str(randint(1, 6)))
+
+# start_handler = CommandHandler('start', start)
+# info_handler = CommandHandler('info', info)
+# roll_handler = CommandHandler('roll', roll)
+
+# dispatcher.add_handler(start_handler)
+# dispatcher.add_handler(info_handler)
+# dispatcher.add_handler(roll_handler)
+# updater.start_polling()
+# updater.idle()
